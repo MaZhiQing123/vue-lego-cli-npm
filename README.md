0b6fd2d95984c5e5bb49cd0eb6a83c01e1b922db
mazhq <mazhq@fintechina.com.cn>
2020-02-25 13:58:20 +0800

    stable

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # vue-lego-cli-npm
+
+**一个可插拔式的vue项目脚手架**
+
+**该脚手架依旧基于webpack来构建**
+
+#### 开始
+
+```bash
+npm i vue-lego-cli-npm -g
+
+vue-lego-cli
+
+npm run add child1 child2 ...
+``` 
+
+
+#### 目录结构
+```bash
+    |-- dist
+    |-- src
+    |   |-- main.js   // 入口文件
+    |   |-- components   // 所有组件
+    |       |-- child   // 所有的业务模块
+    |       |   |-- exampleA   // 例子：模块A
+    |       |   |   |-- index.js   // 注意！必要文件，模块A通过此文件暴露
+    |       |   |   |-- common   // 业务模块下的目录结构其实都可以自定义，只需要在index.js中暴露即可
+    |       |   |   |-- view
+    |       |   |   |   |-- a1.vue,
+    |       |   |   |   |-- a2.vue,
+    |       |-- root   // 根
+    |           |-- App.vue
+    |           |-- main.vue,
+    |           |-- mount.js   // 挂载各个模块
+    |           |-- relation.js   // 模块之间的关联关系
+    |           |-- router
+    |               |-- index.js   // router实例，可以预设一些404，login等等，主要路由是通过mount.js来挂载
+    |-- webpack  // webpack配置
+
+```
--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ const ora = require('ora');
 const inquirer = require('inquirer');
 const { exec } = require('child_process');
 const chalk = require('chalk')
+const fs = require('fs')
 
 const inquirerList = [{
     type: 'input',
@@ -23,50 +24,43 @@ const inquirerList = [{
 
 async function answer(){
     let answerList = await inquirer.prompt(inquirerList)
-    console.log(answerList)
     fs.mkdir(`${process.cwd()}/${answerList.project_name}`,(error) => {
         if(error){
             console.log(error)
             console.log(chalk.red('目录创建失败'))
-        }
-    })
-    const dt = ora('开始下载项目模板...').start();
-    download('github:MaZhiQing123/vue-lego', `${process.cwd()}/${answerList.project_name}/`, function (err) {
-        if(err){
-            console.log(err)
-            dt.fail('模板下载失败')
             return
         }
-        dt.succeed('模板下载完成');
-        const dm = ora('开始下载依赖...').start();
-        exec('cnpm i', (err, stdout, stderr) => {
-            if(err) {
-                console.log(err);
-                dm.fail('依赖下载失败')
-                return;
+        const dt = ora('开始下载项目模板...').start();
+        download('github:MaZhiQing123/vue-lego', `${process.cwd()}/${answerList.project_name}/`, function (err) {
+            if(err){
+                console.log(err)
+                dt.fail('模板下载失败')
+                return
             }
-            dm.succeed('依赖下载完成');
+            dt.succeed('模板下载完成');
 
-            console.log(chalk.green(`
-                cd ${answerList.project_name}
+            const dm = ora('开始下载依赖...').start();
+            exec(`cd ${answerList.project_name} && cnpm i`, (err, stdout, stderr) => {
+                console.log(answerList.project_name)
+                if(err){
+                    console.log(err);
+                    dm.fail('依赖下载失败')
+                    return;
+                }
+                dm.succeed('依赖下载完成');
+    
+                console.log(chalk.green(`
+        cd ${answerList.project_name}
 
-                npm start
+        npm start
 
-                npm run build
-            `))
+        npm run build
+                `))
+            })
+
         })
     })
+
 }
 
 answer()
-// const spinner = ora('开始下载项目模板...').start();
-// // download('github:MaZhiQing123/vue-lego', process.cwd(), function (err) {
-// //     console.log(err ? err : '')
-//     setTimeout(() => {
-//         spinner.color = 'yellow';
-//         spinner.text = 'Loading rainbows';
-//         spinner.succeed('下载完成');
-//     }, 1000);
-
-
-//   })
